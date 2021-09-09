49d9b7339596cc7bc523fef1fbf65e19df9973e3
Markus <markuswohlan@gmail.com>
2021-09-09 19:56:59 +0200

    Get the Firebase config from secret file which is in gitignore

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,16 +1,9 @@
-
+import firebaseConfig from './secret'
 import { initializeApp } from "firebase/app"
 import { getFirestore,serverTimestamp,increment,documentId } from "firebase/firestore";
 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCb3uoTn3503PLaEB7avgnvq1HeTkqQEaY",
-    authDomain: "sentrade-labeling.firebaseapp.com",
-    projectId: "sentrade-labeling",
-    storageBucket: "sentrade-labeling.appspot.com",
-    messagingSenderId: "960291792069",
-    appId: "1:960291792069:web:b9fdba4a2755267826fcac"
-};
+
 
 //init firebase
 
